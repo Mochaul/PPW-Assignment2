3e1ad2947d32f13dd3a9f0c5bc2098b2aa40c451
Mochaul <praditomo@gmail.com>
2017-12-12 01:03:03 +0700

    fix api key

--- sabibatbet_profile/tests.py
+++ sabibatbet_profile/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class sabibatbetProfileUnitTest(TestCase):
+	def test_sabibatbet_profile_url_is_exist(self):
+		response = Client().get('/sabibatbet_profile/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_lab7_using_index_func(self):
+		found = resolve('/sabibatbet_profile/')
+		self.assertEqual(found.func, index)
--- sabibatbet_profile/urls.py
+++ sabibatbet_profile/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import *
+
 #url for app
 urlpatterns = [
+    url(r'^$', index, name='index'),
     url(r'^$', show_profile, name='showProfile'),
 ]
