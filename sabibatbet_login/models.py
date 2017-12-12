22fd4a874a6530dd6a7595748d7150ac8f18db10
Kianutama Radianur <kinu.kianu@gmail.com>
2017-12-12 19:45:41 +0700

    fix user company

--- sabibatbet_login/models.py
+++ sabibatbet_login/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class User(models.Model):
     id = models.CharField('id', max_length=20, primary_key=True, )
     name = models.CharField('Name', max_length=200)
-    company = models.CharField('company', max_length=100)
+    company = models.CharField('company', max_length=100, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
